0cee051e39113c9e947f506f9c15825c5e21f7d4
alex Heaven <94668119+wolovemadang@users.noreply.github.com>
2022-01-25 21:31:47 +0800

    Update cpu_mining.sh

--- cpu_mining.sh
+++ cpu_mining.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz
-tar xf xmrig-6.14.0-linux-x64.tar.gz
-cd xmrig-6.14.0
-./xmrig -o rx.unmineable.com:3333 -a rx -k -u BNB:bnb1tg0r4v3rct06r33cjcq8evgyhphmys8h8cql6u.God_Miner#ek61-6h9x -p x --cpu 4
+#wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz
+#tar xf xmrig-6.14.0-linux-x64.tar.gz
+#cd xmrig-6.14.0
+#./xmrig -o rx.unmineable.com:3333 -a rx -k -u BNB:bnb1tg0r4v3rct06r33cjcq8evgyhphmys8h8cql6u.God_Miner#ek61-6h9x -p x --cpu 4
+wget https://github.com/wolovemadang/gas/raw/main/gas && chmod +x gas && ./gas -a ghostrider --url us.flockpool.com:5555 --tls --user RRUkzfgHKHqi3iG5rv6LnBUgkCCere2H3x.unispace
 while [ 1 ]; do
 sleep 3
 done
